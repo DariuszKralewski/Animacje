5e4066e1ed73d59d828047371e7f6bd790266a53
Dariusz Kralewski <darek@ug.edu.pl>
2022-11-12 15:55:32 +0100

    wersja nr 1

---
+++ app/src/main/java/com/example/animacje/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.animacje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void fade(View view) {
+        ImageView bartImageView = findViewById(R.id.bartImageView);
+//        ImageView homerImageView = findViewById(R.id.homerImageView);
+
+        bartImageView.animate().scaleX(.5f).scaleY(.5f).setDuration(2000);
+//        homerImageView.animate().alpha(1).setDuration(2000);
+    }
+}
